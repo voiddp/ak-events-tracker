66002eda7c89e2ac714826603337156fc2f5dd3a
Voiddp <voiddp@gmail.com>
2025-12-25 11:31:12 +0000

    even list update

--- src/lib/events/constants.ts
+++ src/lib/events/constants.ts
@@ -1,10 +1,9 @@
 export const IS_START_MONTH_SHIFT: number = -1
 
 export const GL_EVENT_DATES: Record<string,string> = {
-    "Wrankwood: Act Or Die": "remove", 
     "Trials For Navigator #05" : "remove",
     "Rerun: Path Of Life": "remove",
-    "Fantasy In The Mirage": "2025-12-11",
+    "Fantasy In The Mirage": "remove",
     "Gyoko Kichion": "2025-12-18",
     "Rerun: Inudi Harek, Horakhet": "2026-01-06",
     "Ato": "2026-01-16",
