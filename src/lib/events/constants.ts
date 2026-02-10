f510b69e1ed935f618fe96f9b8206c59a5078e26
voiddp <32855221+voiddp@users.noreply.github.com>
2026-02-10 12:55:14 +0200

    02-03.2026 datamined dates

--- src/lib/events/constants.ts
+++ src/lib/events/constants.ts
@@ -1,11 +1,12 @@
 export const IS_START_MONTH_SHIFT: number = -1
 
 export const GL_EVENT_DATES: Record<string,string> = {
-    "Trials For Navigator #05" : "remove",
-    "Rerun: Path Of Life": "remove",
-    "Fantasy In The Mirage": "remove",
-    "Gyoko Kichion": "2025-12-18",
-    "Rerun: Inudi Harek, Horakhet": "2026-01-06",
-    "Ato": "2026-01-16",
-    "Battleplan Arclight": "2026-01-23",
+    "Rerun: Inudi Harek, Horakhet": "remove",
+    "Ato": "remove",
+    "Battleplan Arclight": "remove",
+    "Duel Channel: Honeydew": "2026-02-03",
+    "Somniloquium Serenum": "2026-02-10",
+    "Rerun: Ending A Grand Overture","2026-02-24",
+    "Rebuilding Mandate": "2026-03-06",
+    "Abnormal Spectrum": "2026-03-19",
 }
