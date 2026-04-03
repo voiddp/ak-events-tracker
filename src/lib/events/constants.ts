30291794a8469767d4d09a10e3987ef8dc19bac8
Voiddp <voiddp@gmail.com>
2026-04-03 11:30:39 +0000

    current events dates

--- src/lib/events/constants.ts
+++ src/lib/events/constants.ts
@@ -1,12 +1,6 @@
 export const IS_START_MONTH_SHIFT: number = -1
 
 export const GL_EVENT_DATES: Record<string,string> = {
-    "Rerun: Inudi Harek, Horakhet": "remove",
-    "Ato": "remove",
-    "Battleplan Arclight": "remove",
-    "Duel Channel: Honeydew": "2026-02-03",
-    "Somniloquium Serenum": "2026-02-10",
-    "Rerun: Ending A Grand Overture": "2026-02-24",
-    "Rebuilding Mandate": "2026-03-06",
-    "Abnormal Spectrum": "2026-03-19",
+    "Abnormal Spectrum": "remove",
+    "Icebreaker Games: #2 共竞目标": "2026-04-02",
 }
