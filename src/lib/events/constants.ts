f8a094c27fddd3bead7a7b6b9e757bce9372901a
voiddp <32855221+voiddp@users.noreply.github.com>
2026-05-22 13:42:05 +0300

    datamined dates

--- src/lib/events/constants.ts
+++ src/lib/events/constants.ts
@@ -1,13 +1,10 @@
 export const IS_START_MONTH_SHIFT: number = -1
 
 export const GL_EVENT_DATES: Record<string,string> = {
-    "Abnormal Spectrum": "remove",
-    "Tender Moments": "remove",
-    "Icebreaker Games: #2 共竞目标": "2026-04-02",
-    "Rhodes Island Icebreaker Games #2": "2026-04-02",
-    "Retracing Our Steps": "2026-04-14",
-    "Rerun: I Portatori Dei Velluti": "2026-05-07",
-    "Rerun: An Accolade To A Rejuvenation": "2026-05-07",
+    "Rerun: I Portatori Dei Velluti": "remove",
+    "Rerun: An Accolade To A Rejuvenation": "remove",
     "Unrealized Realities": "2026-05-21",    
     "Rerun: Exodus From The Pale Sea": "2026-06-05",
+    "Trials For Navigator #06": "2026-06-11",
+    "Medjehtiqedti Bound": "2026-06-19",
 }
