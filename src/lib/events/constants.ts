fa5e213decb5301eba0c8265703e0ef119b724f3
voiddp <32855221+voiddp@users.noreply.github.com>
2026-06-22 17:28:16 +0300

    datamined dates

--- src/lib/events/constants.ts
+++ src/lib/events/constants.ts
@@ -1,10 +1,10 @@
 export const IS_START_MONTH_SHIFT: number = -1
 
-export const GL_EVENT_DATES: Record<string,string> = {
-    "Rerun: I Portatori Dei Velluti": "remove",
-    "Rerun: An Accolade To A Rejuvenation": "remove",
-    "Unrealized Realities": "2026-05-21",    
-    "Rerun: Exodus From The Pale Sea": "2026-06-05",
+export const GL_EVENT_DATES: Record<string,string> = { 
+    "Rerun: Exodus From The Pale Sea": "remove",
     "Trials For Navigator #06": "2026-06-11",
     "Medjehtiqedti Bound": "2026-06-19",
+    "Such Is The Joy Of Our Reunion": "2026-07-06",
+    "First Of A Thousand Autumns": "2026-07-16",
+    "Vector Breakthrough #2: Misery": "2026-07-30",
 }
